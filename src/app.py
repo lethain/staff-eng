a8557cc3f962b3f930299b8f36efaebe8bd2acd5
Will Larson <lethain@gmail.com>
2020-02-16 14:17:45 -0800

    Add template rendering

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    #return 'Hello {}!\n'.format(target)
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
