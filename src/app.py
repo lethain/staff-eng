1339184e900b09370a69b269b9096a1fed131b21
Will Larson <lethain@gmail.com>
2020-02-16 16:09:30 -0800

    Add templates

--- src/app.py
+++ src/app.py
@@ -5,10 +5,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    #return 'Hello {}!\n'.format(target)
-    return render_template("base.html")
+def front():
+    return render_template("front.html")
+
+@app.route('/stories')
+def stories():
+    return render_template("stories.html")
+
+@app.route('/share')
+def share():
+    return render_template("share.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
