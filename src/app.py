36005a32484d521bb68a935d67508f6855c246e5
Will Larson <lethain@gmail.com>
2020-02-27 07:16:46 -0800

    Add stories/keavy-mcminn

--- src/app.py
+++ src/app.py
@@ -20,6 +20,11 @@ def share():
 def about():
     return render_template("about.html")
 
+@app.route('/stories/<name>')
+def story(name):
+    template_path = "stories/%s.html" % name
+    return render_template(template_path)
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
