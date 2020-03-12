2cd9e8336980340bf59305a978dfdc9507316393
Will Larson <lethain@gmail.com>
2020-03-11 21:16:11 -0700

    Add RSS feed, link to mail that doesn't quite work.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,20 @@
 import os
+import feedgen.feed
 
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
+
+
+class Story:
+    def __init__(self, name, slug, date):
+        self.name = name
+        self.slug = slug
+        self.date = date
+
+STORIES = [
+    Story("Keavy McMinn", "keavy-mcminn", "2020/3/1"),
+]
+        
+    
 
 app = Flask(__name__)
 
@@ -24,6 +38,21 @@ def about():
 def story(name):
     template_path = "stories/%s.html" % name
     return render_template(template_path)
+
+@app.route('/rss')
+def rss():
+    fg = feedgen.feed.FeedGenerator()
+    fg.title("StaffEng")
+    fg.description("RSS feed for StaffEng.com")
+    fg.author({'name': 'Will Larson', 'email': 'lethain@gmail.com'})
+    fg.link(href='https://staffeng.com', rel='alternate')
+    fg.link(href='https://staffeng.com/rss', rel='self')
+    fg.language('en')
+
+    txt =  fg.rss_str(pretty=True)
+    resp = make_response(txt)
+    resp.headers.set('Content-Type', 'application/rss+xml')
+    return resp    
 
 
 if __name__ == "__main__":
