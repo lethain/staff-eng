6a110659adbefd4d07f266e171ecb03c5d378476
Will Larson <lethain@gmail.com>
2020-06-21 14:47:37 -0700

    Gatsby warning messages

--- src/components/header.js
+++ src/components/header.js
@@ -22,7 +22,7 @@ const Header = ({ siteTitle }) => {
     <ul>
       <li><Link to="/stories">Stories</Link></li>
         <li><Link to="/share">Share your story</Link></li>
-        <li><Link to={data.site.siteMetadata.mailingListURL}>Subscribe</Link></li>
+        <li><a href={data.site.siteMetadata.mailingListURL}>Subscribe</a></li>
     </ul>
             </nav>
     )
--- src/pages/share.js
+++ src/pages/share.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
-
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
