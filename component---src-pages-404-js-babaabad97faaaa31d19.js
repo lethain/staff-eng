4e91c6dcdb386ca26d8a42c46cdfcc3a5f801333
lethain <lethain@users.noreply.github.com>
2025-04-20 14:25:38 +0000

    deploy: d2c6b093353f1b549f22fb2ead4948fe6625eacb

--- component---src-pages-404-js-babaabad97faaaa31d19.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(e,t,a){a.r(t);var l=a(7294),s=a(4429),u=a(9357);t.default=()=>l.createElement(s.Z,null,l.createElement(u.Z,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
-//# sourceMappingURL=component---src-pages-404-js-babaabad97faaaa31d19.js.map
